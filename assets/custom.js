88516f9f8d4065bdb6c2233f897e655d4ceb5a7b
Khushwinder-dev24x7 <khushwinder.dev@gmail.com>
2024-11-15 21:58:58 +0530

    Version 1.0.0.0

---
+++ assets/custom.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    // Clone the desktop nav links to the offcanvas menu
+    $('#navLinks').clone().appendTo('#offcanvasNavLinks');
+
+    // // Smooth scroll on clicking navbar links
+    // $('.navbar-nav .nav-link').on('click', function(event) {
+    //     event.preventDefault();
+    //     $('html, body').animate({
+    //         scrollTop: $($(this).attr('href')).offset().top - 50 // Offset to account for navbar height
+    //     }, 500);
+    //     // Close offcanvas if open
+    //     if ($('.offcanvas').hasClass('show')) {
+    //         $('.offcanvas').offcanvas('hide');
+    //     }
+    // });
+
+    // // Add active class to navbar link based on scroll position
+    // $(window).on('scroll', function() {
+    //     var scrollPos = $(window).scrollTop() + 60; // Offset for navbar height
+    //     $('.navbar-nav .nav-link').each(function() {
+    //         var sectionOffset = $($(this).attr('href')).offset().top;
+    //         if (scrollPos >= sectionOffset) {
+    //             $('.navbar-nav .nav-link').removeClass('active-link');
+    //             $(this).addClass('active-link');
+    //         }
+    //     });
+    // });
+
+
+     // Get video and mute/unmute button
+     const video = document.getElementById('videoBanner');
+     const muteUnmuteBtn = document.getElementById('muteUnmuteBtn');
+
+     // Mute/Unmute functionality
+     muteUnmuteBtn.addEventListener('click', () => {
+         if (video.muted) {
+             video.muted = false;
+             muteUnmuteBtn.innerHTML = '<i class="fas fa-volume-up fs-6"></i>'; // Change icon to volume-up
+         } else {
+             video.muted = true;
+             muteUnmuteBtn.innerHTML = '<i class="fas fa-volume-mute fs-6"></i>'; // Change icon to volume-mute
+         }
+     });
+
+
+
+
+}); // Document Get Ready Close Here
